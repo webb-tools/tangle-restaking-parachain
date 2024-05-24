98df9681ec5d1df134c7ecac47fcdfb0858ae43c
Salman Pathan <pathansalman555@gmail.com>
2024-05-24 10:51:19 +0530

    fix imports and yarn format (#13)

--- types/scripts/updateMetadata.ts
+++ types/scripts/updateMetadata.ts
@@ -15,30 +15,30 @@ const fs = require('fs');
 const { w3cwebsocket: WS } = pkg;
 
 const main = (): void => {
-  const endpoint = 'ws://127.0.0.1:8844';
+	const endpoint = 'ws://127.0.0.1:8844';
 
-  console.log('Connecting to ', endpoint);
-  const ws = new WS(endpoint);
+	console.log('Connecting to ', endpoint);
+	const ws = new WS(endpoint);
 
-  ws.onerror = (error: any) => {
-    console.error('WebSocket error:', error);
-  };
+	ws.onerror = (error: any) => {
+		console.error('WebSocket error:', error);
+	};
 
-  ws.onopen = (): void => {
-    ws.send('{"id":"1","jsonrpc":"2.0","method":"state_getMetadata","params":[]}');
-    console.log("data send");
-  };
+	ws.onopen = (): void => {
+		ws.send('{"id":"1","jsonrpc":"2.0","method":"state_getMetadata","params":[]}');
+		console.log('data send');
+	};
 
-  ws.onmessage = (msg: any): void => {
-    const fullData = JSON.parse(msg.data);
-    const metadata = fullData.result;
+	ws.onmessage = (msg: any): void => {
+		const fullData = JSON.parse(msg.data);
+		const metadata = fullData.result;
 
-    fs.writeFileSync('./src/metadata/static-latest.ts', `export default '${metadata}'`);
-    fs.writeFileSync('./src/metadata/metadata.json', JSON.stringify(fullData, null, 2));
+		fs.writeFileSync('./src/metadata/static-latest.ts', `export default '${metadata}'`);
+		fs.writeFileSync('./src/metadata/metadata.json', JSON.stringify(fullData, null, 2));
 
-    console.log('Done');
-    process.exit(0);
-  };
+		console.log('Done');
+		process.exit(0);
+	};
 };
 
 main();
